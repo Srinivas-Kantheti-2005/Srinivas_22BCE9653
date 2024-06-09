2b59dc3029f5a88037d03327c51083faa52456b3
kanth <srinivaskantheti394@gmail.com>
2024-06-09 12:35:36 +0530

    Convert java file to kotlin

--- app/src/main/java/com/example/srinivas_22bce9653/kotlinexs/Example.kt
+++ app/src/main/java/com/example/srinivas_22bce9653/kotlinexs/Example.kt
@@ -2,4 +2,12 @@ package com.example.srinivas_22bce9653.kotlinexs
 
 fun main() {
     println("Hello World!");
+    var myStudent: Student = Student("Srinivas", 20, "Thullur")
+    println(myStudent.name)
+    myStudent.name="Srinvias Kantheti"
+    println(myStudent.name)
+
+    var myEmployee: Employee = Employee("Titas", 23, "Kantheru")
+    println(myEmployee.getName())
+    println(myEmployee.name)
 }
