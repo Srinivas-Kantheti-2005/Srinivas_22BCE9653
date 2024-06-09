3b4e18dfc9b19697727266012d36b0a4cb1bad04
kanth <srinivaskantheti394@gmail.com>
2024-06-09 12:53:42 +0530

    colors.xml

--- app/src/main/java/com/example/srinivas_22bce9653/kotlinexs/Example.kt
+++ app/src/main/java/com/example/srinivas_22bce9653/kotlinexs/Example.kt
@@ -2,6 +2,8 @@ package com.example.srinivas_22bce9653.kotlinexs
 
 fun main() {
     println("Hello World!");
+    var androidTeacher: Teacher = Teacher("Bhargavi", 15, "Guntur")
+
     var myStudent: Student = Student("Srinivas", 20, "Thullur")
     println(myStudent.name)
     myStudent.name="Srinvias Kantheti"
