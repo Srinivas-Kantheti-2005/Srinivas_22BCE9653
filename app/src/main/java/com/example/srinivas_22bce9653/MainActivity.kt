848db75c57bd192fe1f279d791be1b2de074f640
kanth <srinivaskantheti394@gmail.com>
2024-06-09 15:20:26 +0530

    Multi language supporing

--- app/src/main/java/com/example/srinivas_22bce9653/MainActivity.kt
+++ app/src/main/java/com/example/srinivas_22bce9653/MainActivity.kt
@@ -4,11 +4,25 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Context
+import android.content.res.Configuration
+import android.widget.Toast
+import java.util.Locale
+
+fun updateLocale(context: Context, language: String) {
+    val locale = Locale(language)
+    Locale.setDefault(locale)
+    val config = Configuration()
+    config.setLocale(locale)
+    context.resources.updateConfiguration(config, context.resources.displayMetrics)
+}
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +34,37 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val username = findViewById<TextView>(R.id.username)
+        val password = findViewById<TextView>(R.id.password)
+        val loginbtn = findViewById<Button>(R.id.loginbtn)
+        val changelanguage = findViewById<Button>(R.id.changelanguage)
+
+        loginbtn.setOnClickListener {
+            clickHandler(username.text.toString(), password.text.toString())
+        }
+
+        changelanguage.setOnClickListener {
+            updateLocale(this, "te")
+            restartActivity()
+        }
     }
 
-    fun clickHandler(view: View) {
+    private fun restartActivity() {
+        val intent = intent
+        finish()
+        startActivity(intent)
+    }
+
+    private fun clickHandler(username: String, password: String) {
         Log.i("MainActivity-clickhandler", "button clicked")
-
-        var dailIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel: 7075322769"))
-
-        var webIntent: Intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/"))
-        startActivity(webIntent)
+        if(username == "Srinivas" && password == "7075322769") {
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            val dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:7075322769"))
+            val webIntent: Intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/"))
+            startActivity(dialIntent)
+        } else {
+            Toast.makeText(this, "Login Failed!!!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
