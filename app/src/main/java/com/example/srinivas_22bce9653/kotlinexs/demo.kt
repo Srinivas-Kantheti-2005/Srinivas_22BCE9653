d20540220210234edea72885adcbd6ea41463931
kanth <srinivaskantheti394@gmail.com>
2024-06-09 12:09:10 +0530

    Kotlin example

---
+++ app/src/main/java/com/example/srinivas_22bce9653/kotlinexs/demo.kt
@@ -0,0 +1,13 @@
+package com.example.srinivas_22bce9653.kotlinexs
+
+fun main() {
+    val numberOfFish = 50
+    val numberOfPlants = 23
+
+    if(numberOfFish > numberOfPlants) {
+        println("Good ratio!")
+    }
+    else {
+        println("Unhealthy ratio")
+    }
+}
