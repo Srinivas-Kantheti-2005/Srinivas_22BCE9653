4f8723fc9d33487d9d7d1d60203c38effd8b8db4
kanth <srinivaskantheti394@gmail.com>
2024-06-14 17:11:40 +0530

    recyclerview part-3 -- implementing the suggestions

--- app/src/main/java/com/example/srinivas_22bce9653/wordsAdapter.kt
+++ app/src/main/java/com/example/srinivas_22bce9653/wordsAdapter.kt
@@ -1,6 +1,29 @@
 package com.example.srinivas_22bce9653
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class wordsAdapter: RecyclerView.Adapter<wordsViewHolder> {
+class wordsAdapter: RecyclerView.Adapter<wordsAdapter.wordsViewHolder>() {
+
+    // Sumanth -- buy row planks from market.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): wordsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    // Daya -- keep counting the no. of items in the dataset.
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    // Lokesh -- write the data on the row plank given by
+    override fun onBindViewHolder(holder: wordsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+    // Titas -- maintain the reserve planks in the holder box.
+    class wordsViewHolder(itemView: View) : ViewHolder(itemView) {}
 }
