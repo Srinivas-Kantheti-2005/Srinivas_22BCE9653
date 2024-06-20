220baaef336132106e3660e05804461b8f48c190
kanth <srinivaskantheti394@gmail.com>
2024-06-20 20:36:54 +0530

    recyclerview with images from json parsed from a webservice

--- app/src/main/java/com/example/srinivas_22bce9653/MarsAdapter.kt
+++ app/src/main/java/com/example/srinivas_22bce9653/MarsAdapter.kt
@@ -3,17 +3,20 @@ package com.example.srinivas_22bce9653
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.example.srinivas_22bce9653.network.MarsPhoto
 
 class MarsAdapter(var listMarsPhoto: List<MarsPhoto>) :RecyclerView.Adapter<MarsAdapter.MarsViewHolder>() {
     class MarsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        var textView:TextView = itemView.findViewById(android.R.id.text1)
+        var textView:TextView = itemView.findViewById(R.id.tvUrl)
+        var marsImageView: ImageView = itemView.findViewById(R.id.ivPhoto)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsViewHolder {
-        var rowPlank = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1,parent,false)
+        var rowPlank = LayoutInflater.from(parent.context).inflate(R.layout.row_layout,parent,false)
         return MarsViewHolder(rowPlank)
     }
 
@@ -22,6 +25,8 @@ class MarsAdapter(var listMarsPhoto: List<MarsPhoto>) :RecyclerView.Adapter<Mars
     }
 
     override fun onBindViewHolder(holder: MarsViewHolder, position: Int) {
-        holder.textView.setText(listMarsPhoto.get(position).imgSrc)
+        var url: String = listMarsPhoto.get(position).imgSrc
+        holder.textView.setText(url)
+        holder.marsImageView.load(url)
     }
 }
